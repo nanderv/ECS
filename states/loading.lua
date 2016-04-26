0bb39835c80f74fecde76500a5844ddc93a88930
nanderv <nanderv@gmail.com>
2016-04-26 21:18:37 +0200

    Simplified loading.lua, fewer planets

--- states/loading.lua
+++ states/loading.lua
@@ -3,42 +3,30 @@ loading.loaded = 1
 -- Loading screen phases, split up loading code among these phases
 loading.phases = {
 function()
-
+        -- reset global game state
         core.reset_game()
+
+        -- add required systems
         core.system.add( require 'systems.velocity_from_acceleration', "update")
         core.system.add( require 'systems.position_from_velocity', "update")
         core.system.add( require 'systems.acceleration_from_mass', "update")
         core.system.add( require 'systems.draw_from_mass_and_position', "draw")
+
+        -- require entity types
         require 'entities.planet'
+
+        -- add entities
         entity = core.entity.add(get_new_planet(300,300,333))
         entity.color={red=255,green=255,blue=255}
         --entity.mass.counts_as_infinite=true
         entity = core.entity.add(get_new_planet(200,300,1))
-        entity.velocity.y = 300
+        entity.velocity.y = 500
 
         entity = core.entity.add(get_new_planet(400,300,1))
-        entity.velocity.y = -300
-        entity.velocity.x = -10
-
-        entity = core.entity.add(get_new_planet(300,100,1))
-        entity.velocity.x = 10
+        entity.velocity.y = -500
+        
 
 
-        entity = core.entity.add(get_new_planet(500,300,1))
-        entity.velocity.y = -300
-        entity.velocity.x = -10
-
-        entity = core.entity.add(get_new_planet(600,300,1))
-        entity.velocity.y = -300
-        entity.velocity.x = -10
-
-        entity = core.entity.add(get_new_planet(700,300,1))
-        entity.velocity.y = 300
-        entity.velocity.x = 10
-
-        entity = core.entity.add(get_new_planet(700,300,1))
-        entity.velocity.y = 300
-        entity.velocity.x = 10
 
 
 
